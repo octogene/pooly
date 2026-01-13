6b275231ad2787d3ce59411c82cf4c2d71639906
Octogene <ooctogene@gmail.com>
2026-01-13 16:26:40 +0100

    wip: common:db

---
+++ common/db/build.gradle.kts
@@ -0,0 +1,24 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.jetbrains.kotlin.jvm)
+    alias(libs.plugins.kotlin.plugin.serialization)
+}
+java {
+    sourceCompatibility = JavaVersion.VERSION_24
+    targetCompatibility = JavaVersion.VERSION_24
+}
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_24
+        freeCompilerArgs.add("-Xcontext-parameters")
+    }
+}
+
+dependencies {
+    implementation(project(":common:core"))
+    implementation(libs.bundles.exposed)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(platform(libs.arrow.stack))
+    implementation(libs.arrow.core)
+}
