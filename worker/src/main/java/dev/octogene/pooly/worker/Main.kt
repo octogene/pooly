5e6d2d08eaf084c2104c7bcbb7830155032063e9
Octogene <ooctogene@gmail.com>
2026-01-19 16:47:33 +0100

    wip(worker): improve db initialization

--- worker/src/main/java/dev/octogene/pooly/worker/Main.kt
+++ worker/src/main/java/dev/octogene/pooly/worker/Main.kt
@@ -1,7 +1,10 @@
 package dev.octogene.pooly.worker
 
 import arrow.continuations.SuspendApp
+import arrow.resilience.Schedule
+import arrow.resilience.retry
 import ch.qos.logback.classic.Logger
+import dev.octogene.pooly.common.db.checkDatabaseInitialization
 import dev.octogene.pooly.common.db.di.repositoriesModule
 import dev.octogene.pooly.ptgraph.api.PoolTogetherGraphQLClient
 import dev.octogene.pooly.rpc.PoolTogetherRPCClient
@@ -11,6 +14,7 @@ import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import org.koin.java.KoinJavaComponent.getKoin
 import org.slf4j.LoggerFactory
+import kotlin.time.Duration.Companion.seconds
 
 fun main() = SuspendApp {
     val logger = LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME)
@@ -41,14 +45,25 @@ fun main() = SuspendApp {
                 }
                 // Dummy
                 single<(String) -> String>(named("password-hasher")) {
-                    {
-                            rawPassword ->
+                    { rawPassword ->
                         rawPassword
                     }
                 }
             }
         )
     }
-    val worker = getKoin().get<Worker>()
-    worker.run()
+    val retrySchedule = Schedule.recurs<Throwable>(3)
+        .and(Schedule.spaced(20.seconds))
+    val database = getKoin().get<Database>()
+    try {
+        retrySchedule.retry<Throwable, Unit> {
+            checkDatabaseInitialization(database)
+        }
+        logger.info("Database initialized successfully.")
+        val worker = getKoin().get<Worker>()
+        worker.run()
+    } catch (e: Exception) {
+        logger.error("Failed to initialize database after multiple retries: ${e.message}", e)
+        throw e
+    }
 }
