74e7bc52eb645c9e8e83c707c7be01c1079fe99d
Octogene <ooctogene@gmail.com>
2026-02-03 09:57:48 +0100

    wip(cache): switch to Lettuce
    Re.This has some bugs that needs to be investigated

--- server/src/main/java/dev/octogene/pooly/server/cache/ValkeyCacheClient.kt
+++ server/src/main/java/dev/octogene/pooly/server/cache/ValkeyCacheClient.kt
@@ -1,30 +1,41 @@
 package dev.octogene.pooly.server.cache
 
+import arrow.core.Either
 import arrow.core.Option
+import arrow.core.left
 import arrow.core.none
+import arrow.core.raise.either
+import arrow.core.right
 import arrow.core.toOption
-import eu.vendeli.rethis.ReThis
-import eu.vendeli.rethis.command.serde.get
-import eu.vendeli.rethis.command.serde.set
-import eu.vendeli.rethis.shared.request.string.SetExpire
+import io.lettuce.core.ExperimentalLettuceCoroutinesApi
+import io.lettuce.core.RedisClient
+import io.lettuce.core.SetArgs
 import kotlinx.serialization.KSerializer
+import kotlinx.serialization.json.Json
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import kotlin.time.Clock
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.minutes
 import kotlin.time.Instant
+import kotlin.time.toJavaInstant
 
 class ValkeyCacheClient(
-    private val client: ReThis,
+    private val lettuceClient: RedisClient,
+    private val json: Json,
     val defaultTTL: Duration = 10.minutes,
     val logger: Logger = LoggerFactory.getLogger(ValkeyCacheClient::class.java)
 ) : CacheClient {
 
+    private val connection = lettuceClient.connect()
+
     override suspend fun <T : Any> get(key: String, serializer: KSerializer<T>): Option<T> {
         logger.debug("Getting value for key {}", key)
         return try {
-            client.get(key = key, serializer).toOption()
+            connection.sync().get(key)?.let { data ->
+                logger.debug("Getting string of ${data.length} length")
+                json.decodeFromString(deserializer = serializer, data)
+            }.toOption()
         } catch (e: Exception) {
             logger.error("Failed to fetch $key: {}", e.message)
             none()
@@ -40,6 +51,7 @@ class ValkeyCacheClient(
         set(key, value, Clock.System.now().plus(ttl), type)
     }
 
+    @OptIn(ExperimentalLettuceCoroutinesApi::class)
     override suspend fun <T : Any> set(
         key: String,
         value: T,
@@ -48,16 +60,29 @@ class ValkeyCacheClient(
     ) {
         logger.debug("Setting value for key {} (expireAt: {})", key, expireAt)
         try {
-            client.set(
-                key = key,
-                value = value,
-                serializer = type,
-                options = arrayOf(
-                    SetExpire.ExAt(expireAt),
-                )
+            connection.sync().set(
+                key,
+                json.encodeToString(serializer = type, value),
+                SetArgs.Builder.exAt(expireAt.toJavaInstant())
             )
         } catch (e: Exception) {
             logger.error("Failed to set $key : {}", e.message)
+        }
+    }
+
+    @OptIn(ExperimentalLettuceCoroutinesApi::class)
+    // TODO: Improve arrow usage
+    override suspend fun clearByPattern(pattern: String): Either<Throwable, Long> = either {
+        return try {
+            val keys = connection.sync().keys(pattern)
+            keys?.let { keys ->
+                logger.debug("Clearing ${keys?.size ?: 0} keys")
+                connection.sync().del(*keys.toTypedArray())
+            }
+            keys.size.toLong().right()
+        } catch (e: Throwable) {
+            logger.error("Failed to clear cache : {}", e.message)
+            e.left()
         }
     }
 }
