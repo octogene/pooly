04ef3d155a50d9a8288eec256ee9cd82536b4811
Octogene <ooctogene@gmail.com>
2026-02-12 16:05:56 +0100

    style(common:mobile): detekt format

--- common/mobile/src/main/java/dev/octogene/pooly/common/mobile/model/Vault.kt
+++ common/mobile/src/main/java/dev/octogene/pooly/common/mobile/model/Vault.kt
@@ -1,8 +1,3 @@
 package dev.octogene.pooly.common.mobile.model
 
-data class Vault(
-    val name: String,
-    val address: String,
-    val symbol: String,
-    val decimals: Int
-)
+data class Vault(val name: String, val address: String, val symbol: String, val decimals: Int)
